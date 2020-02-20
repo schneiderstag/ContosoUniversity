d49a97052d71f69775191ff0dac7602838e44432
Bruno <brunopiasso@hotmail.com>
2020-02-20 19:46:41 +0000

    Initial commit

---
+++ ContosoUniversity/Models/Instructor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Instructor : Person
+    {
+        [DataType(DataType.Date), Display(Name = "Hire Date"), DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime HireDate { get; set; }
+
+        public virtual ICollection<Course> Courses { get; set; }
+        public virtual OfficeAssignment OfficeAssignment { get; set; }
+    }
+}
---
+++ ContosoUniversity/Models/Person.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public abstract class Person
+    {
+        /* This tutorial demonstrates how to implement TPH (table-per-hierarchy) inheritance. 
+        TPH is the default inheritance pattern in the Entity Framework, 
+        so all you have to do is create a Person class, 
+        change the Instructor and Student classes to derive from Person, 
+        add the new class to the DbContext, and create a migration. */
+
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
+        public string FirstMidName { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
+    }
+}
